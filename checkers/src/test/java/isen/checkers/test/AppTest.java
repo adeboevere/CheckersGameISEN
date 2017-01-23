d72dec6d3e7d06a970a78bba9a05f34e6c7abab6
Theophane MARTEAu <theophane.marteau@isen-lille.fr>
2017-01-23 15:18:43 +0100

    Ajout des classes test

--- checkers/src/test/java/isen/checkers/test/AppTest.java
+++ checkers/src/test/java/isen/checkers/test/AppTest.java
@@ -1,15 +1,86 @@
 package isen.checkers.test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
 
 public class AppTest {
-	
+
 	@Test
-	public void createPlayerTest(){
+	public void testPlay() {
 		
 	}
+
+	@Test
+	public void testFill() {
+		
+	}
+
+	@Test
+	public void testMouseListen() {
+		
+	}
+
+	@Test
+	public void testInvokeGameOver() {
+	}
+
+	@Test
+	public void testDisplayImage() {
+	}
+
+	@Test
+	public void testMove() {
+	}
+
+	@Test
+	public void testDecrementPieces() {
+	}
+
+	@Test
+	public void testInvertMove() {
+	}
+
+	@Test
+	public void testCheckCapture() {
+	}
+
+	@Test
+	public void testCheckQueenCapture() {
+	}
+
+	@Test
+	public void testCheckCaptureIntInt() {
+	}
+
+	@Test
+	public void testSetup() {
+		
+	}
+
+	@Test
+	public void testSetBlack() {
+	}
+
+	@Test
+	public void testSetWhite() {
+		
+	}
+
+	@Test
+	public void testPaintComponentGraphics() {
+		
+	}
+
+	@Test
+	public void testSameColor() {
+		
+	}
+
+	@Test
+	public void testOppositeColor() {
 	
+	}
 
 }
--- checkers/src/test/java/isen/checkers/test/PlayerTest.java
+++ checkers/src/test/java/isen/checkers/test/PlayerTest.java
@@ -1,5 +1,20 @@
 package isen.checkers.test;
 
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
 public class PlayerTest {
+
+	@Test
+	public void testValid() {
+		
+	}
+
+	@Test
+	public void testOppositeColor() {
+		
+	}
 
 }
